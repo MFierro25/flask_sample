b4393093f05971022df43230bd7201495f23ed33
Miguel Fierro <88460070+MFierro25@users.noreply.github.com>
2022-02-04 00:16:54 -0800

    added python functions to html template

--- tutorial.py
+++ tutorial.py
@@ -2,9 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=["Mariners", "Seahawks", "Krakken"], x=Teams)
 
 # @app.route("/<name>/")
 # def user(name):
@@ -16,8 +16,3 @@ def home():
 
 if __name__ == "__main__":
     app.run()
-
-
-
-
-
